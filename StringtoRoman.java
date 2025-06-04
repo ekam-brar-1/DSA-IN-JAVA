b85c7345d6e16d5de8ad1863bd3df39f33bb2079
Ekamjot Singh <160436305+ekam-brar-1@users.noreply.github.com>
2025-06-04 13:17:10 -0600

    StringToRoman

---
+++ StringtoRoman.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public String intToRoman(int num) {
+       
+        int[]    values  = {1000, 900, 500, 400, 100,  90,  50,  40,  10,   9,   5,   4,   1};
+        String[] symbols = {"M",  "CM","D", "CD","C", "XC","L", "XL","X", "IX","V", "IV","I"};
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < values.length && num > 0; i++) {
+            int count = num / values[i];
+            
+            for (int k = 0; k < count; k++) {
+                sb.append(symbols[i]);
+            }
+            num %= values[i];
+        }
+        
+        return sb.toString();
+    }
+}
